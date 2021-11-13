434b699c04e0cf3470d0bf4c8206e0ad74ed23fd
USER <srrajib61@gmail.com>
2021-11-13 16:10:52 +0600

    int

--- lib/chat.dart
+++ lib/chat.dart
@@ -20,7 +20,6 @@ class _ChaState extends State<Cha> {
               centerTitle: true,
               flexibleSpace: FlexibleSpaceBar(
                 title: Text('shahinur Rahaman'),
-
               ),
               pinned: true,
               expandedHeight: 200,
@@ -28,7 +27,6 @@ class _ChaState extends State<Cha> {
             ),
             SliverList(
               delegate: SliverChildListDelegate(<Widget>[
-
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: ElevatedButton(
@@ -38,7 +36,14 @@ class _ChaState extends State<Cha> {
                             builder: (context) {
                               return AlertDialog(
                                 title: Text('Alert'),
-                                content: Text('My Name Is .......'),
+                                content: Positioned(
+                                  top: -50,
+                                  child: CircleAvatar(
+                                    radius: 50,
+                                    // backgroundColor: Colors.yellow,
+                                    child: Image.asset('assets/images.jpg'),
+                                  ),
+                                ),
                                 actions: [
                                   ElevatedButton(
                                       onPressed: () {
@@ -51,8 +56,73 @@ class _ChaState extends State<Cha> {
                       },
                       child: Text("Rajib")),
                 ),
-                ListTile(
-                  title: Text("Rajib"),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return Dialog(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20))),
+                                child: Stack(
+                                  alignment: Alignment.topCenter,
+                                  clipBehavior: Clip.none,
+                                  children: <Widget>[
+                                    Container(
+                                        height: 300,
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              SizedBox(
+                                                height: 10,
+                                              ),
+                                              Text('Warning:'),
+                                              SizedBox(
+                                                height: 40,
+                                              ),
+                                              RichText(text: TextSpan(
+                                                text: "Rich Text Test",style: TextStyle(fontSize: 20,
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.cyan
+                                              ),
+                                                children: [
+                                                  TextSpan(
+                                                    text: "Second rich text Test",
+                                                        style: TextStyle(
+                                                          fontSize: 20,
+                                                          fontWeight: FontWeight.bold,
+                                                          color: Colors.red
+                                                        )
+                                                  ),
+                                                ]
+                                              ))
+                                            ],
+                                          ),
+                                        )),
+                                    Positioned(
+                                        top: -50,
+                                        child: CircleAvatar(
+                                          radius: 50,
+                                          child: ClipRRect(
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(50)),
+                                            child: Image.asset(
+                                                'assets/images.jpg'),
+                                          ),
+                                        ))
+                                  ],
+                                ),
+                              );
+                            });
+                      },
+                      child: Text("Rajib")),
                 ),
                 ListTile(
                   title: Text("Rajib"),
